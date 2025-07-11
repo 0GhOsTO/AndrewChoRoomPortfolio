0cd4f81d3017108643d50f3d7169b7c2d872bced
Andrew Cho <cho.yoonho023@gmail.com>
2025-07-11 11:34:29 -0400

    Glass material updated

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ const sizes = {
     width: window.innerWidth,
 };
 
+const yAxisFan = [];
+
+const raycasterObjects = [];
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 //Loaders
 const textureLoader = new THREE.TextureLoader();
 
@@ -109,7 +114,7 @@ Object.entries(textureMap).forEach(([key, paths]) => {
 });
 
 const videoElement = document.createElement("video");
-videoElement.src = "/textures/room/video/screen.mp4";
+videoElement.src = "/textures/video/screen.mp4";
 videoElement.loop = true;
 videoElement.muted = true;
 videoElement.playsInline = true;
@@ -120,7 +125,12 @@ const videoTexture = new THREE.VideoTexture(videoElement);
 videoTexture.colorspace = THREE.SRGBColorSpace;
 videoTexture.flipY = false;
 
-loader.load("/models/Room_Portfolio.glb", (glb)=>{
+window.addEventListener("mousemove", (e) => {
+    pointer.x = (e.clientX/window.innerWidth) * 2 -1;
+    pointer.y = -(e.clientY/window.innerHeight) * 2 +1;
+});
+
+loader.load("/models/Room_Portfolio_Ray.glb", (glb)=>{
     glb.scene.traverse((child) =>{
         if(child.isMesh){
             if(child.name.includes("Glass")){
@@ -149,12 +159,21 @@ loader.load("/models/Room_Portfolio.glb", (glb)=>{
                         });
                         child.material = material;
 
+                        if(child.name.includes("Fan")){
+                            yAxisFan.push(child);
+                        }
+
                         if(child.material.map){
                             child.material.map.minFilter = THREE.LinearFilter;
                         }
                     }
                 });
             }
+
+            if(child.name.includes("Button")){
+                raycasterObjects.push(child);
+            }
+
         }
     });
 
@@ -184,7 +203,8 @@ const controls = new OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 controls.update();
-controls.target.set(1.5279505254337087, 3.7166745685785796, 1.1424439609332728);
+controls.target.set(-1.8610304303839955, 2.3808504327544835, -1.9583995416260147);
+
 
 //Event listener
 window.addEventListener( 'resize', () => {
@@ -210,13 +230,22 @@ const render = () => {
     // Change the name of the button through blender
     // Delete some of the useless sides of the walls.
     // Download the simple mp4 file and save it somewhere so that I have an access to it.
+    yAxisFan.forEach(fan=>{
+        fan.rotation.y += 0.01;
+    });
 
-    // const origin = new THREE.Vector3();
-    // object.getWorldPosition(origin);
-    // console.log("Current rotation origin (default):", origin);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    raycaster.setFromCamera(pointer,camera);
+    const intersects = raycaster.intersectObjects(raycasterObjects);
+    for(let i = 0; i<intersects.length;i++){
+        intersects[i].object.material.color.set(0xff0000);
+    }
+
+    if(intersects.length>0){
+        document.body.style.cursor = "pointer";
+    }else{
+        document.body.style.cursor = "default";
+    }
 
     renderer.render( scene, camera );
 
