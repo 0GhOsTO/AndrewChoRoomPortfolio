55435cce964a9fe7d4125a21244413b983f40635
Andrew Cho <cho.yoonho023@gmail.com>
2025-07-16 22:52:22 -0400

    night vision needs to be updated.

--- src/main.js
+++ src/main.js
@@ -4,11 +4,19 @@ import { OrbitControls } from '/src/utils/OrbitControls.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import gsap from "gsap"
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { SSAARenderPass } from 'three/examples/jsm/postprocessing/SSAARenderPass.js';
+import {SRGBColorSpace} from "three";
+import { OutputPass } from 'three/examples/jsm/postprocessing/OutputPass.js';
+
+
+
 
 window.mobileCheck = function() {
     let check = false;
     (function (a) {
-        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
     })(navigator.userAgent || navigator.vendor || window.opera);
     console.log("###Yes, Checking if it's mobile or not")
     return check;
@@ -37,6 +45,8 @@ const modals = {
     resume: document.querySelector(".modal.resume"),
     project: document.querySelector(".modal.project"),
 };
+
+
 
 
 //Touching the modal exit button
@@ -86,6 +96,7 @@ document.querySelector(".sunny-side").addEventListener("click", (e) => {
         ){
             child.material = isNight ? child.userData.nightMaterial : child.userData.dayMaterial;
             child.material.map.minFilter = THREE.LinearFilter;
+            child.material.map.magFilter = THREE.LinearFilter;
         }
     });
 });
@@ -225,6 +236,7 @@ const loadedTextures = {
     night: {},
 };
 
+//control the colorspace
 Object.entries(textureMap).forEach(([key, paths]) => {
     const dayTexture = textureLoader.load(paths.day);
     dayTexture.flipY = false;
@@ -246,7 +258,7 @@ videoElement.autoplay = true;
 videoElement.play();
 
 const videoTexture = new THREE.VideoTexture(videoElement);
-videoTexture.colorspace = THREE.SRGBColorSpace;
+// videoTexture.colorSpace = THREE.SRGBColorSpace;
 videoTexture.flipY = false;
 
 window.addEventListener("mousemove", (e) => {
@@ -297,9 +309,8 @@ function handleRaycasterInteraction() {
 
 window.addEventListener("click", handleRaycasterInteraction);
 
-loader.load("/models/Room_Portfolio_Please_Last.glb", (glb)=>{
+loader.load("/models/Room_Portfolio_UV.glb", (glb)=>{
     glb.scene.traverse((child) =>{
-
         if(child.isMesh){
             if(child.name.includes("RayCaster")){
                 raycasterObjects.push(child);
@@ -307,7 +318,6 @@ loader.load("/models/Room_Portfolio_Please_Last.glb", (glb)=>{
                 child.userData.initialPosition = new THREE.Vector3().copy(child.position);
                 child.userData.initialRotation = new THREE.Vector3().copy(child.rotation);
             }
-
             if(child.name.includes("ChairTop")){
                 child.rotation.y = -Math.PI/30;
                 gsap.to(child.rotation,{
@@ -347,7 +357,7 @@ loader.load("/models/Room_Portfolio_Please_Last.glb", (glb)=>{
                         });
 
                         child.userData.nightMaterial = new THREE.MeshBasicMaterial({
-                            map: loadedTextures.night[key]
+                            map: loadedTextures.night[key],
                         });
 
                         child.material = child.userData.dayMaterial;
@@ -358,6 +368,7 @@ loader.load("/models/Room_Portfolio_Please_Last.glb", (glb)=>{
 
                         if(child.material.map){
                             child.material.map.minFilter = THREE.LinearFilter;
+                            child.material.map.magFilter = THREE.LinearFilter;
                         }
                     }
                 });
@@ -367,7 +378,6 @@ loader.load("/models/Room_Portfolio_Please_Last.glb", (glb)=>{
 
         }
     });
-
     scene.add(glb.scene);
 });
 
@@ -385,16 +395,31 @@ if(isMobile){
     camera.position.set( 9.341425508289262, 6.582700064329998, 9.4580936212497 );
 }
 
-const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true} );
+const renderer = new THREE.WebGLRenderer({canvas: canvas} );
 renderer.setSize( sizes.width, sizes.height );
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.outputColorSpace = THREE.SRGBColorSpace; // Already good
 
+const composer = new EffectComposer(renderer);
+composer.outputColorSpace = THREE.SRGBColorSpace; // << ADD THIS
+
+const ssaaPass = new SSAARenderPass(scene, camera);
+ssaaPass.sampleLevel = 4; // 0 to 5; try 4 or 5 for quality
+ssaaPass.unbiased = true;
+composer.addPass(ssaaPass);
+
+//Convert the ColorCode
+const outputPass = new OutputPass();
+outputPass.colorSpace = THREE.SRGBColorSpace;
+composer.addPass(outputPass);
+
+//Need to be deleted
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-
+//orbit control
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.minPolarAngle = 0;
 controls.maxPolarAngle = Math.PI / 2;
@@ -553,7 +578,7 @@ function playHoverAnimation(object, isHovering){
     }
 }
 
-const render = () => {
+const render = deltaTime => {
     controls.update();
 
     // console.log(camera.position); // [x, y, z]
@@ -567,14 +592,11 @@ const render = () => {
         fan.rotation.y += 0.01;
     });
 
-
-
     //RayCaster
     if(!isModalOpen) {
         raycaster.setFromCamera(pointer, camera);
 
         currentIntersects = raycaster.intersectObjects(raycasterObjects);
-
 
         if (currentIntersects.length > 0) {
             const currentIntersectObject = currentIntersects[0].object;
@@ -604,11 +626,8 @@ const render = () => {
         }
     }
 
-    renderer.render( scene, camera );
-
+    composer.render();
     window.requestAnimationFrame( render );
 };
-
-
 
 render();
